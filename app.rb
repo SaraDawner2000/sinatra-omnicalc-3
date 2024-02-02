a79534016dbf030dc2dadcfd686af8d9e15d5a35
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-02-02 16:31:51 +0000

    fetch latitude and longitude

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
   erb("<h1>Welcome to Omnicalc 3</h1>")
@@ -11,6 +13,14 @@ get("/umbrella") do
 end
 
 get("/process_umbrella") do
+
+  @user_loc = params[:user_loc]
+
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@user_loc}&key=#{ENV.fetch(GMAPS_KEY)}"
+  raw_gmaps_data = HTTP.get(gmaps_url)
+  parsed_gmaps_data = JSON.parse(raw_gmaps_data)
+  @longitude = parsed_gmaps_data["results"][0]["geometry"]["location"]["lng"]
+  @latitude = parsed_gmaps_data["results"][0]["geometry"]["location"]["lat"]
   erb(:process_umbrella)
 end
 
